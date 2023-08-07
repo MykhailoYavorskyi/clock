b3822bccd73049b64d945095921211912e3e857b
MykhailoYavorskyi <my70035@gmail.com>
2023-08-07 22:11:56 +0200

    another bug fixed

--- script.js
+++ script.js
@@ -12,6 +12,31 @@ const refs = {
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+const currentDate = new Date();
+
+const day = currentDate.getDay();
+const date = currentDate.getDate();
+const month = currentDate.getMonth();
+const year = currentDate.getFullYear();
+const time = currentDate.toLocaleTimeString("en-GB");
+
+refs.day.textContent = `Day of the week: ${days[day]}`;
+refs.date.textContent = `Day of the month: ${date}`;
+refs.month.textContent = `Month: ${months[month]}`;
+refs.year.textContent = `Year: ${year}`;
+refs.clock.textContent = `Time: ${time}`;
+
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+const seconds = currentDate.getSeconds();
+const secondsDeg = 6 * seconds;
+const minutesDeg = 6 * minutes;
+const hoursDeg = 30 * hours + 0.5 * minutes;
+
+refs.seconds.style.transform = `translateX(-50%) rotate(${secondsDeg}deg)`;
+refs.minutes.style.transform = `translateX(-50%) rotate(${minutesDeg}deg)`;
+refs.hours.style.transform = `translateX(-50%) rotate(${hoursDeg}deg)`;
+
 setInterval(() => {
   const currentDate = new Date();
 
