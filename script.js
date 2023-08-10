de39314ec2e93287003c27dc36fc33e686177e34
MykhailoYavorskyi <my70035@gmail.com>
2023-08-10 18:28:05 +0200

    fixed code duplication

--- script.js
+++ script.js
@@ -12,34 +12,16 @@ const refs = {
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-const currentDate = new Date();
-
-const day = currentDate.getDay();
-const date = currentDate.getDate();
-const month = currentDate.getMonth();
-const year = currentDate.getFullYear();
-const time = currentDate.toLocaleTimeString("en-GB");
-
-refs.day.textContent = `Day of the week: ${days[day]}`;
-refs.date.textContent = `Day of the month: ${date}`;
-refs.month.textContent = `Month: ${months[month]}`;
-refs.year.textContent = `Year: ${year}`;
-refs.clock.textContent = `Time: ${time}`;
-
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-const seconds = currentDate.getSeconds();
-const secondsDeg = 6 * seconds;
-const minutesDeg = 6 * minutes;
-const hoursDeg = 30 * hours + 0.5 * minutes;
-
-refs.seconds.style.transform = `translateX(-50%) rotate(${secondsDeg}deg)`;
-refs.minutes.style.transform = `translateX(-50%) rotate(${minutesDeg}deg)`;
-refs.hours.style.transform = `translateX(-50%) rotate(${hoursDeg}deg)`;
+setCalendar();
+setClock();
 
 setInterval(() => {
-  const currentDate = new Date();
+  setCalendar();
+  setClock();
+}, 1000);
 
+function setCalendar() {
+  const currentDate = new Date();
   const day = currentDate.getDay();
   const date = currentDate.getDate();
   const month = currentDate.getMonth();
@@ -51,7 +33,10 @@ setInterval(() => {
   refs.month.textContent = `Month: ${months[month]}`;
   refs.year.textContent = `Year: ${year}`;
   refs.clock.textContent = `Time: ${time}`;
+}
 
+function setClock() {
+  const currentDate = new Date();
   const hours = currentDate.getHours();
   const minutes = currentDate.getMinutes();
   const seconds = currentDate.getSeconds();
@@ -62,4 +47,4 @@ setInterval(() => {
   refs.seconds.style.transform = `translateX(-50%) rotate(${secondsDeg}deg)`;
   refs.minutes.style.transform = `translateX(-50%) rotate(${minutesDeg}deg)`;
   refs.hours.style.transform = `translateX(-50%) rotate(${hoursDeg}deg)`;
-}, 1000);
+}
